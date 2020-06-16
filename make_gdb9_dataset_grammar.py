afa737095aca7b110c68170fd3fc0b4d2fd3bc04
Sebastian Pölsterl <sebp@k-d-w.org>
2020-06-16 12:02:32 +0200

    Update path to grammar

--- make_gdb9_dataset_grammar.py
+++ make_gdb9_dataset_grammar.py
@@ -46,6 +46,6 @@ for i in trange(0, len(L), 100):
     onehot = to_one_hot(L[i:i+100])
     OH[i:i+100,:,:] = onehot
 
-h5f = h5py.File('gdb9_grammar_dataset.h5','w')
+h5f = h5py.File('data/gdb9_grammar_dataset.h5','w')
 h5f.create_dataset('data', data=OH)
 h5f.close()
